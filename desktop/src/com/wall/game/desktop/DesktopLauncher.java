85e264416db3eb94c01d2a4d4a534a9ea32185dd
danielwall <daniel.wall@ocsbstudent.ca>
2017-12-29 15:24:51 -0500

    got libgdx project files

---
+++ desktop/src/com/wall/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wall.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wall.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
